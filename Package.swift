45659b5940f9dcc667dcb0636b7af444c3a174ea
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-03-03 13:53:05 +0100

    The egg just hatched!

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+  name: "Colibri",
+  products: [
+    .executable(name: "colibri", targets: ["colibri"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(name: "colibri", dependencies: []),
+
+    .target(name: "ColibriLib", dependencies: ["Sema"]),
+    .target(name: "AST", dependencies: []),
+    .target(name: "Parser", dependencies: ["AST"]),
+    .target(name: "Sema", dependencies: ["AST"]),
+
+    .target(name: "Utils", dependencies: []),
+  ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Colibri
+
+Colibri is a cousin of the [Swift programming language](https://swift.org).
