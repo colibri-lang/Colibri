e12f4bd299004bc13135fa20c92aefe7aecc618f
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-03-08 20:16:32 +0100

    Implement an AST node for unresolved declaration references

--- Sources/AST/Expr.swift
+++ Sources/AST/Expr.swift
@@ -1,3 +1,21 @@
-public protocol Expr {
+public protocol Expr: SourceRepresentable {
   
+}
+
+/// A reference to a value whose declaration has yet to be resolved.
+///
+/// This reoresents an unresolved identifier which may refer to a declaration that will be resolved
+/// during sema, or simply an unknown identifier.
+public final class UnresolvedDeclRefExpr: Expr {
+
+  /// The name of the identifier.
+  public let name: String
+
+  public let range: SourceRange?
+
+  public init(name: String, range: SourceRange?) {
+    self.name = name
+    self.range = range
+  }
+
 }
